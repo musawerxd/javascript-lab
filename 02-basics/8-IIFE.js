3c68a96ec64e8beab689c81793b48a54bc62a452
musawerxd <hunnyshah0012@gmail.com>
2026-02-07 22:57:21 +0500

    .\02-basics\8-IIFE.js

--- 02-basics/8-IIFE.js
+++ 02-basics/8-IIFE.js
@@ -3,4 +3,55 @@
 
 
 
-// JS  Notes 07/02/2026
+// JS IIFE (Imediately Invoked Function Expression) Notes 07/02/2026
+
+
+
+// 1. Syntax
+console.log(`\n Syntax`);
+(function () {
+    console.log("Imediately Invoked! no call is needed");
+})();
+
+
+
+
+
+// 2. With Parameters
+console.log(`\n 2. With Parameters`);
+(function (name) {
+    console.log(`Hello ${name}`)
+})("Ali");
+
+
+
+
+
+// 3. Arrow IIFE
+console.log(`\n 3. Arrow IIFE`);
+(() => {
+    console.log("Arrow IIFE!");
+})();
+
+
+
+
+
+// 4. can be Named
+console.log(`\n4. can be Named`);
+(function greet() {
+    console.log("salam");
+})();
+
+// greet(); // error , because IIFE cannot be called, runs just for once 
+
+
+
+
+
+// Interesting
+console.log(`\n Interesting`);
+function imediate() { console.log("i ran imediate") }
+imediate() //then why IIFE
+//Because IIFE runs instantly like a normal call,
+// but unlike a normal function it doesn’t stay in memory or pollute global scope.
